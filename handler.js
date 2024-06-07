a1d76e3ebe54e39eac87afe3617e94c6b4411c62
Aldrin Jerome Almacin <aldrinjerome19@gmail.com>
2024-06-07 00:32:04 -0600

    Working lambda function for openai

--- handler.js
+++ handler.js
@@ -1,17 +1,45 @@
+'use strict'
+const OpenAI = require("openai");
+const instructions = require('./instructions.json');
+
+const openai = new OpenAI(process.env.OPENAI_API_KEY);
+exports.home = async (event) => {
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: "Welcome to the Sumelu API",
+        }),
+    };
+}
+
 exports.globalLearn = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Global Learn",
-    }),
-  };
+    if (!event.pathParameters || !event.pathParameters.term) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({
+                message: "Please provide a term",
+            }),
+        };
+    }
+
+    const term = event.pathParameters.term;
+
+    const completion = await openai.chat.completions.create({
+        messages: [{role: "system", content: instructions.instructions}, {role: "user", content: term}],
+        model: "gpt-4o",
+    });
+
+    return {
+        statusCode: 200,
+        body: completion.choices[0].message.content,
+    };
 };
 
 exports.globalDomains = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: "Global Domains",
-    }),
-  };
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            message: "Global Domains",
+        }),
+    };
 };
